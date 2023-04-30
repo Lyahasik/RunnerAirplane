a8e72948cfb39d8b3bca475e5f479a41fdb0ad0d
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2023-04-30 20:11:13 +0300

    added boss bmp

--- Assets/Project/Scripts/Gameplay/Bosses/BossBMP.cs
+++ Assets/Project/Scripts/Gameplay/Bosses/BossBMP.cs
@@ -4,31 +4,24 @@ using RunnerAirplane.Gameplay.Weapons;
 
 namespace RunnerAirplane.Gameplay.Bosses
 {
-    public class BossMiniTank : MonoBehaviour
+    public class BossBMP : MonoBehaviour
     {
         [SerializeField] private float _rechargeAttack;
         [SerializeField] private float _delayStartAttack;
         
         [Space]
-        [SerializeField] private HomingRocketLauncher _homingRocketLauncher;
+        [SerializeField] private MachineGun _machineGun;
         [SerializeField] private float _timeAttack1;
         private float _timeStartAttack1;
         private float _timeEndAttack1;
         private bool _isActiveAttack1;
         
         [Space]
-        [SerializeField] private Bombardment _bombardment;
+        [SerializeField] private HomingRocketLauncher _homingRocketLauncher;
         [SerializeField] private float _timeAttack2;
         private float _timeStartAttack2;
         private float _timeEndAttack2;
         private bool _isActiveAttack2;
-        
-        [Space]
-        [SerializeField] private LaserGun _laserGun;
-        [SerializeField] private float _timeAttack3;
-        private float _timeStartAttack3;
-        private float _timeEndAttack3;
-        private bool _isActiveAttack3;
 
         private void Awake()
         {
@@ -42,21 +35,17 @@ namespace RunnerAirplane.Gameplay.Bosses
             
             StartFire2();
             TryEndAttack2();
-            
-            StartFire3();
-            TryEndAttack3();
         }
 
         private void StartFire1()
         {
             if (_isActiveAttack1
                 || _isActiveAttack2
-                || _isActiveAttack3
                 || _timeStartAttack1 > Time.time
                 || _timeStartAttack1 == 0f)
                 return;
-
-            _homingRocketLauncher.IsActive = true;
+            
+            _machineGun.IsActive = true;
             _isActiveAttack1 = true;
             
             _timeEndAttack1 = Time.time + _timeAttack1;
@@ -68,7 +57,7 @@ namespace RunnerAirplane.Gameplay.Bosses
                 || _timeEndAttack1 > Time.time)
                 return;
             
-            _homingRocketLauncher.IsActive = false;
+            _machineGun.IsActive = false;
             _isActiveAttack1 = false;
 
             _timeStartAttack1 = 0f;
@@ -79,12 +68,11 @@ namespace RunnerAirplane.Gameplay.Bosses
         {
             if (_isActiveAttack1
                 || _isActiveAttack2
-                || _isActiveAttack3
                 || _timeStartAttack2 > Time.time
                 || _timeStartAttack2 == 0f)
                 return;
             
-            _bombardment.IsActive = true;
+            _homingRocketLauncher.IsActive = true;
             _isActiveAttack2 = true;
             
             _timeEndAttack2 = Time.time + _timeAttack2;
@@ -95,40 +83,12 @@ namespace RunnerAirplane.Gameplay.Bosses
             if (!_isActiveAttack2
                 || _timeEndAttack2 > Time.time)
                 return;
-
-            _bombardment.IsActive = false;
+            
+            _homingRocketLauncher.IsActive = false;
             _isActiveAttack2 = false;
             
-            _timeStartAttack2 = 0f;
-            _timeStartAttack3 = Time.time + _rechargeAttack;
-        }
-
-        private void StartFire3()
-        {
-            if (_isActiveAttack1
-                || _isActiveAttack2
-                || _isActiveAttack3
-                || _timeStartAttack3 > Time.time
-                || _timeStartAttack3 == 0f)
-                return;
-            
-            _laserGun.IsActive = true;
-            _isActiveAttack3 = true;
-            
-            _timeEndAttack3 = Time.time + _timeAttack3;
-        }
-        
-        private void TryEndAttack3()
-        {
-            if (!_isActiveAttack3
-                || _timeEndAttack3 > Time.time)
-                return;
-
-            _laserGun.IsActive = false;
-            _isActiveAttack3 = false;
-            
-            _timeStartAttack3 = 0f;
             _timeStartAttack1 = Time.time + _rechargeAttack;
+            _timeStartAttack2 = 0f;
         }
     }
 }
