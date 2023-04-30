a9544809ba20d0cce148513552354b1e5aa08b6f
Lyahasik <48154267+Lyahasik@users.noreply.github.com>
2023-04-30 15:32:14 +0300

    added boss space fighter

---
+++ Assets/Project/Scripts/Gameplay/Bosses/BossSpaceFighter.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+using RunnerAirplane.Gameplay.Weapons;
+
+namespace RunnerAirplane
+{
+    public class BossSpaceFighter : MonoBehaviour
+    {
+        [SerializeField] private float _rechargeAttack;
+        [SerializeField] private float _delayStartAttack;
+        
+        [Space]
+        [SerializeField] private MachineGun _machineGun;
+        [SerializeField] private float _timeAttack1;
+        private float _timeStartAttack1;
+        private float _timeEndAttack1;
+        private bool _isActiveAttack1;
+        
+        [Space]
+        [SerializeField] private HomingRocketLauncher _homingRocketLauncher;
+        [SerializeField] private float _timeAttack2;
+        private float _timeStartAttack2;
+        private float _timeEndAttack2;
+        private bool _isActiveAttack2;
+
+        private void Awake()
+        {
+            _timeStartAttack1 = Time.time + _delayStartAttack;
+        }
+
+        private void Update()
+        {
+            StartFire1();
+            TryEndAttack1();
+            
+            StartFire2();
+            TryEndAttack2();
+        }
+
+        private void StartFire1()
+        {
+            if (_isActiveAttack1
+                || _isActiveAttack2
+                || _timeStartAttack1 > Time.time
+                || _timeStartAttack1 == 0f)
+                return;
+            
+            _machineGun.IsActive = true;
+            _isActiveAttack1 = true;
+            
+            _timeEndAttack1 = Time.time + _timeAttack1;
+        }
+
+        private void TryEndAttack1()
+        {
+            if (!_isActiveAttack1
+                || _timeEndAttack1 > Time.time)
+                return;
+            
+            _machineGun.IsActive = false;
+            _isActiveAttack1 = false;
+
+            _timeStartAttack1 = 0f;
+            _timeStartAttack2 = Time.time + _rechargeAttack;
+        }
+
+        private void StartFire2()
+        {
+            if (_isActiveAttack1
+                || _isActiveAttack2
+                || _timeStartAttack2 > Time.time
+                || _timeStartAttack2 == 0f)
+                return;
+            
+            _homingRocketLauncher.IsActive = true;
+            _isActiveAttack2 = true;
+            
+            _timeEndAttack2 = Time.time + _timeAttack2;
+        }
+        
+        private void TryEndAttack2()
+        {
+            if (!_isActiveAttack2
+                || _timeEndAttack2 > Time.time)
+                return;
+            
+            _homingRocketLauncher.IsActive = false;
+            _isActiveAttack2 = false;
+            
+            _timeStartAttack1 = Time.time + _rechargeAttack;
+            _timeStartAttack2 = 0f;
+        }
+    }
+}
